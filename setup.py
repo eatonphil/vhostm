830232ad184008cc36dbce7263572bb1b35fa45c
Phil Eaton <philneaton95@gmail.com>
2016-01-18 23:39:34 -0500

    bumps version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="vhostm",
     packages=["vhostm"],
-    version="0.1",
+    version="1.0",
     description="Manage nginx virtual servers and hosts file entries.",
     author="Phil Eaton",
     author_email="me@eatonphil.com",
     url="https://github.com/eatonphil/vhostm",
-    download_url="https://github.com/eatonphil/vhostm/tarball/0.1",
+    download_url="https://github.com/eatonphil/vhostm/tarball/1.0",
     keywords=["nginx", "virtual", "hosts"],
     install_requires=[
         "jinja2>=2.8"
